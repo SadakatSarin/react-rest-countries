8fd0cbb6132c41f263138e122158837fe52254b0
unknown <SadakatSarin>
2024-01-19 21:19:02 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <LaodCountries></LaodCountries>
 
     </div>
   );
 }
 
-function laodCountries() {
+function LaodCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res=>res.json())
+    .then(data=>setCountries(data))
+
+  },[])
+
+  return (
   
+    <div>
+
+      <h1>countries</h1>
+      <h3>available countries:{ countries.length}</h3>
+
+    </div>
+  );
 
 
-  
 }
-
 
 
 
